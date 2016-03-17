49ecdee4ed5e20f123a15d120c130a419df9b684
Scott Prue <prescottprue@gmail.com>
2016-03-17 07:51:49 -0400

    Version v0.0.1
    * Unit tests added and passing
    * Stage-0 babel

--- test/unit/index.spec.js
+++ test/unit/index.spec.js
@@ -1,8 +1,21 @@
-/* global describe, it, expect */
-describe('redux-fireuser Library', () => {
-  describe('constructor', () => {
-    it('has unit test scaffolding', () => {
-      expect(1).to.equal(1)
+/* global chai, describe, it, expect */
+import middleware from '../../src/middleware'
+
+describe('matter middleware', () => {
+  const doDispatch = () => {}
+  const doGetState = () => {}
+  const nextHandler = middleware({dispatch: doDispatch, getState: doGetState})
+  it('must return a function to handle next', () => {
+    chai.assert.isFunction(nextHandler)
+    chai.assert.strictEqual(nextHandler.length, 1)
+  })
+  describe.skip('handle errors', () => {
+    it('must throw if argument is non-object', done => {
+      try {
+        middleware()
+      } catch (err) {
+        done()
+      }
     })
   })
 })
